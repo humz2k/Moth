785fabece9eaa59a17c3e1ca3e4e2144fedebe95
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2023-04-14 22:34:09 -0500

    adf

--- test_moth_tmp.cpp
+++ test_moth_tmp.cpp
@@ -2,10 +2,10 @@
 #include "moth_lib.hpp"
 
 __Mothint Mothmain(){
-__Mothstr a;
-a = "Hello World!";
-a = __MothBasePLUS(a,"1");
-__MothBaseINDEX(a,1,0) = 'T';
+__MothArray<__Mothint> a(2);
+a.init(2,2);
+__MothArrayINDEX(a,2,0,0) = 1;
+__MothArrayINDEX(a,2,1,1) = 1;
 __MothPrint(a);
 return 0;
 
