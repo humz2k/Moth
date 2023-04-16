c3f2d454f4ac96dfbe0292d9b17d8e8814d650d1
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2023-04-16 00:58:08 -0500

    ads

--- test_moth_tmp.cpp
+++ test_moth_tmp.cpp
@@ -1,19 +1,9 @@
 
 #include "moth_lib.hpp"
 
-template <class test,class test2>
-__Mothvoid Mothtest(__Mothint a){
-test ab;test2 abc;
-ab = a;
-abc = 100.5;
-__MothPrint(ab);
-__MothPrint("\n");__MothPrint(abc);
-
-}
 __Mothint Mothmain(){
-__MothArray<__Mothint> fuckyou;
-fuckyou = newArray<__Mothint>(1,10);
-Mothtest<__Mothint,__Mothfloat>(5);
+
+__MothPrint(newArray<__Mothint>(1,10));
 return 0;
 
 }
