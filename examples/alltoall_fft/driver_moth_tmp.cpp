41259eedc94ba42c08bbb9c75c2a56b0e2e61909
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2023-04-19 18:56:32 -0500

    aa

--- examples/alltoall_fft/driver_moth_tmp.cpp
+++ examples/alltoall_fft/driver_moth_tmp.cpp
@@ -4,7 +4,6 @@
 
 #include <mpi.h>
 
-
 #include <fftw3.h>
 
 
@@ -639,186 +638,6 @@ __MothmpiMothbarrier(comm_world);
 
 
 };
-__Mothfloat __MothmathMothacosf(__Mothfloat inp){
-return acosf(inp);
-
-}
-
-__Mothdouble __MothmathMothacos(__Mothdouble inp){
-return acos(inp);
-
-}
-
-__Mothfloat __MothmathMothasinf(__Mothfloat inp){
-return asinf(inp);
-
-}
-
-__Mothdouble __MothmathMothasin(__Mothdouble inp){
-return asin(inp);
-
-}
-
-__Mothfloat __MothmathMothatanf(__Mothfloat inp){
-return atanf(inp);
-
-}
-
-__Mothdouble __MothmathMothatan(__Mothdouble inp){
-return atan(inp);
-
-}
-
-__Mothfloat __MothmathMothatan2f(__Mothfloat inp1,__Mothfloat inp2){
-return atan2f(inp1,inp2);
-
-}
-
-__Mothdouble __MothmathMothatan2(__Mothdouble inp1,__Mothdouble inp2){
-return atan2(inp1,inp2);
-
-}
-
-__Mothfloat __MothmathMothcosf(__Mothfloat inp){
-return cosf(inp);
-
-}
-
-__Mothdouble __MothmathMothcos(__Mothdouble inp){
-return cos(inp);
-
-}
-
-__Mothfloat __MothmathMothcoshf(__Mothfloat inp){
-return coshf(inp);
-
-}
-
-__Mothdouble __MothmathMothcosh(__Mothdouble inp){
-return cosh(inp);
-
-}
-
-__Mothfloat __MothmathMothsinf(__Mothfloat inp){
-return sinf(inp);
-
-}
-
-__Mothdouble __MothmathMothsin(__Mothdouble inp){
-return sin(inp);
-
-}
-
-__Mothfloat __MothmathMothsinhf(__Mothfloat inp){
-return sinhf(inp);
-
-}
-
-__Mothdouble __MothmathMothsinh(__Mothdouble inp){
-return sinh(inp);
-
-}
-
-__Mothfloat __MothmathMothtanhf(__Mothfloat inp){
-return tanhf(inp);
-
-}
-
-__Mothdouble __MothmathMothtanh(__Mothdouble inp){
-return tanh(inp);
-
-}
-
-__Mothfloat __MothmathMothexpf(__Mothfloat inp){
-return expf(inp);
-
-}
-
-__Mothdouble __MothmathMothexp(__Mothdouble inp){
-return exp(inp);
-
-}
-
-__Mothfloat __MothmathMothlogf(__Mothfloat inp){
-return logf(inp);
-
-}
-
-__Mothdouble __MothmathMothlog(__Mothdouble inp){
-return log(inp);
-
-}
-
-__Mothfloat __MothmathMothlog10f(__Mothfloat inp){
-return log10f(inp);
-
-}
-
-__Mothdouble __MothmathMothlog10(__Mothdouble inp){
-return log10(inp);
-
-}
-
-__Mothfloat __MothmathMothpowf(__Mothfloat inp1,__Mothfloat inp2){
-return powf(inp1,inp2);
-
-}
-
-__Mothdouble __MothmathMothpow(__Mothdouble inp1,__Mothdouble inp2){
-return pow(inp1,inp2);
-
-}
-
-__Mothfloat __MothmathMothfabsf(__Mothfloat inp){
-return fabsf(inp);
-
-}
-
-__Mothdouble __MothmathMothfabs(__Mothdouble inp){
-return fabs(inp);
-
-}
-
-__Mothfloat __MothmathMothfmodf(__Mothfloat inp1,__Mothfloat inp2){
-return fmodf(inp1,inp2);
-
-}
-
-__Mothdouble __MothmathMothfmod(__Mothdouble inp1,__Mothdouble inp2){
-return fmod(inp1,inp2);
-
-}
-
-__Mothfloat __MothmathMothsqrtf(__Mothfloat inp){
-return sqrtf(inp);
-
-}
-
-__Mothdouble __MothmathMothsqrt(__Mothdouble inp){
-return sqrt(inp);
-
-}
-
-__Mothfloat __MothmathMothceilf(__Mothfloat inp){
-return ceilf(inp);
-
-}
-
-__Mothdouble __MothmathMothceil(__Mothdouble inp){
-return ceil(inp);
-
-}
-
-__Mothfloat __MothmathMothfloorf(__Mothfloat inp){
-return floorf(inp);
-
-}
-
-__Mothdouble __MothmathMothfloor(__Mothdouble inp){
-return floor(inp);
-
-}
-
 class __MothObjectplan{
 public:
 fftw_plan raw;__Mothvoid Moth__init__(){
@@ -927,7 +746,6 @@ dfft.Mothforward(buff1,buff2);
 if (__MothBaseEQUAL(dist.world_rank,0)){
 __MothPrint(convString("K-SPACE\n"));
 __MothPrint(convString("Real in: ["));__MothPrint(__MothnmMothmin(__MothGetSlice(buff1,6,0,buff1.dims.get()[0],1,0,0,0)));__MothPrint(convString(","));__MothPrint(__MothnmMothmax(__MothGetSlice(buff1,6,0,buff1.dims.get()[0],1,0,0,0)));__MothPrint(convString("], Imag in: ["));__MothPrint(__MothnmMothmin(__MothGetSlice(buff1,6,0,buff1.dims.get()[0],1,1,1,0)));__MothPrint(convString(","));__MothPrint(__MothnmMothmax(__MothGetSlice(buff1,6,0,buff1.dims.get()[0],1,1,1,0)));__MothPrint(convString("]\n"));
-__MothPrint(convString("[0,0,0] = "));__MothPrint(__MothArrayINDEX(buff1,2,0,0));__MothPrint(convString(" + "));__MothPrint(__MothArrayINDEX(buff1,2,0,1));__MothPrint(convString("i\n"));
 
 }
 
