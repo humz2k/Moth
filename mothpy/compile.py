26aef0b33b003bcf1a148d4dbad00b1a876fcb62
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2023-06-08 22:46:18 -0500

    chatgpt documentation lol

--- mothpy/compile.py
+++ mothpy/compile.py
@@ -3,58 +3,136 @@ from .parsing import *
 import os
 from llvmlite import binding
 
+def compile_moth(filename, llvm_compiler="llc", optimization_level=3, threaded=False, n_threads=5, echo_compile=False):
+    """
+    Compiles a Moth source file to an object file.
 
-def compileMoth(fname,ll = "llc",opt = 3,threaded = False, nthreads = 5,echo_compile=False):
-    path = os.path.dirname(os.path.abspath(__file__)) + "/tokens.txt"
-    tokens = lex(fname,path)
-    parsed = parse(tokens,path)
-    module = parsed.eval(fname,threaded,nthreads)
-    out = "\n".join([i for i in str(module).split("\n") if not i.startswith("target")])
-    triple = binding.get_default_triple()
-    out = 'target triple = "' + triple + '"\n' + out
-    tmp_ll = fname + ".tmp.ll"
-    with open(tmp_ll,"w") as f:
-        f.write(out)
-    opt_ll = fname + ".tmp.opt.ll" #-polly-vectorizer=polly
-    #os.system('opt ' + tmp_ll + " -passes='always-inline' -S -o " + opt_ll)
-    if echo_compile:
-        print('opt ' + tmp_ll + " -passes='always-inline,mem2reg' -S -o " + opt_ll)
-    os.system('opt ' + tmp_ll + " -passes='always-inline,mem2reg' -S -o " + opt_ll)
-    tmp_o = fname + ".tmp.o"
-    if echo_compile:
-        print(ll + " -filetype=obj " + opt_ll + " -o " + tmp_o + " -O" + str(opt))
-    os.system(ll + " -filetype=obj " + opt_ll + " -o " + tmp_o + " -O" + str(opt))
-    return tmp_o,opt_ll,tmp_ll
+    Parameters
+    ----------
+    filename : str
+        The path to the Moth source file to compile.
 
-def link(fnames,out,is_shared,c,cc="gcc-13",gc_tools=None,c_tools = None,file_tools = None,threading_tools = None,threaded = False, nthreads = 5, echo_compile=False, mpi_tools = None):
-    if type(gc_tools) == type(None):
-        gc_tools = os.path.dirname(os.path.abspath(__file__)) + "/gc_tools.c"
-    if type(c_tools) == type(None):
-        c_tools = os.path.dirname(os.path.abspath(__file__)) + "/c_tools.c"
-    if type(file_tools) == type(None):
-        file_tools = os.path.dirname(os.path.abspath(__file__)) + "/file_tools.c"
-    if type(mpi_tools) == type(None):
-        mpi_tools = os.path.dirname(os.path.abspath(__file__)) + "/mpi_tools.c"
-    if threaded:
-        if type(threading_tools) == type(None):
-            threading_tools = os.path.dirname(os.path.abspath(__file__)) + "/threading_tools.c"
+    llvm_compiler : str, optional
+        The LLVM compiler to use (default is 'llc').
+
+    optimization_level : int, optional
+        The optimization level to use (default is 3).
+
+    threaded : bool, optional
+        Whether to use multi-threading (default is False).
+
+    n_threads : int, optional
+        The number of threads to use (default is 5).
+
+    echo_compile : bool, optional
+        Whether to print the compile commands (default is False).
+
+    Returns
+    -------
+    tuple
+        Paths to the generated object file, the optimized LLVM file, and the LLVM file.
+    """
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tokens.txt")
+    tokens = lex_file(filename, path)
+    parsed = parse(tokens, path)
+    module = parsed.eval(filename, threaded, n_threads)
+    
+    output_code = "\n".join(line for line in str(module).split("\n") if not line.startswith("target"))
+    target = 'target triple = "' + binding.get_default_triple() + '"\n'
+    output_code = target + output_code
+    
+    tmp_ll_path = filename + ".tmp.ll"
+    with open(tmp_ll_path, "w") as file:
+        file.write(output_code)
+        
+    opt_ll_path = filename + ".tmp.opt.ll"
+    command = f'opt {tmp_ll_path} -passes="always-inline,mem2reg" -S -o {opt_ll_path}'
+    if echo_compile:
+        print(command)
+    os.system(command)
+    
+    tmp_obj_path = filename + ".tmp.o"
+    command = f'{llvm_compiler} -filetype=obj {opt_ll_path} -o {tmp_obj_path} -O{optimization_level}'
+    if echo_compile:
+        print(command)
+    os.system(command)
+    
+    return tmp_obj_path, opt_ll_path, tmp_ll_path
+
+def link_files(filepaths, output_filepath, is_shared, compile_only=False, compiler="gcc-13",
+               garbage_collector_tools=None, c_tools=None, file_tools=None, threading_tools=None, 
+               threaded=False, n_threads=5, echo_compile=False, mpi_tools=None):
+    """
+    Links several files together into a single output file.
+
+    Parameters
+    ----------
+    filepaths : list of str
+        The paths to the files to link.
+
+    output_filepath : str
+        The path to the output file to create.
+
+    is_shared : bool
+        Whether to create a shared library.
+
+    compile_only : bool, optional
+        Whether to stop after compilation (default is False).
+
+    compiler : str, optional
+        The compiler to use (default is 'gcc-13').
+
+    garbage_collector_tools : str, optional
+        The path to the garbage collector tools file (default is None).
+
+    c_tools : str, optional
+        The path to the C tools file (default is None).
+
+    file_tools : str, optional
+        The path to the file tools file (default is None).
+
+    threading_tools : str, optional
+        The path to the threading tools file (default is None).
+
+    threaded : bool, optional
+        Whether to use multi-threading (default is False).
+
+    n_threads : int, optional
+        The number of threads to use (default is 5).
+
+    echo_compile : bool, optional
+        Whether to print the compile commands (default is False).
+
+    mpi_tools : str, optional
+        The path to the MPI tools file (default is None).
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    if garbage_collector_tools is None:
+        garbage_collector_tools = os.path.join(base_dir, "gc_tools.c")
+    if c_tools is None:
+        c_tools = os.path.join(base_dir, "c_tools.c")
+    if file_tools is None:
+        file_tools = os.path.join(base_dir, "file_tools.c")
+    if mpi_tools is None:
+        mpi_tools = os.path.join(base_dir, "mpi_tools.c")
+
+    if threaded and threading_tools is None:
+        threading_tools = os.path.join(base_dir, "threading_tools.c")
     else:
         threading_tools = ""
-    gc_a = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','bdwgc','libgc.a'))
-    fnames += [gc_tools,c_tools,gc_a,file_tools,threading_tools]
-    files = " ".join(fnames)
-    shared = ""
-    if is_shared:
-        shared = " -shared"
-    c_str = " -O3"
-    if c:
-        c_str += " -c"
-    if threaded:
-        c_str += " -pthread"
-    if "mpi" in cc:
-        files += " " + mpi_tools
-    #print(cc + " " + files + " -o " + out + shared)
-    #print(cc + " " + files + " -o " + out + shared + c_str)
+
+    gc_lib_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','bdwgc','libgc.a'))
+
+    filepaths += [garbage_collector_tools, c_tools, gc_lib_path, file_tools, threading_tools]
+    files_str = " ".join(filepaths)
+
+    shared_flag = " -shared" if is_shared else ""
+    compile_flag = " -O3" + (" -c" if compile_only else "")
+    thread_flag = " -pthread" if threaded else ""
+    mpi_flag = " " + mpi_tools if "mpi" in compiler else ""
+
+    command = f'{compiler} {files_str} {mpi_flag} -o {output_filepath} {shared_flag} {compile_flag} {thread_flag}'
     if echo_compile:
-        print(cc + " " + files + " -o " + out + shared + c_str)
-    os.system(cc + " " + files + " -o " + out + shared + c_str)
+        print(command)
+    os.system(command)
--- mothpy/lexing/lex.py
+++ mothpy/lexing/lex.py
@@ -2,140 +2,329 @@ from rply import LexerGenerator
 import re
 import os
 
-def remove_comments(raw):
-    multilines = re.findall(r"\#\#\#.*?\#\#\#",raw,re.DOTALL)
-    for i in multilines:
-        raw = raw.replace(i,(i.count("\n")) * "\n")
-    comments = re.findall(r"\#.*",raw)
-    for i in comments:
-        raw = raw.replace(i,"")
-    return raw
+def remove_comments(code_str):
+    """
+    This function removes all the single-line and multi-line comments from a given piece of Moth code.
 
-def convert(raw):
-    lines = raw.splitlines()
+    Parameters
+    ----------
+    code_str : str
+        A string representing the Moth code from which to remove comments.
 
-    indent_level = 0
-    out = ""
-    for line in lines:
-        if len(line.strip()) != 0:
-            indents = line.count("    ")
-            if indents > indent_level:
-                out += ("{" * (indents - indent_level))
-                indent_level = indents
-                out += line
-                if line.strip()[-1] != ":":
-                    if line.strip()[-1] == ";":
-                        pass
-                    else:
-                        out += ";"
-            elif indents < indent_level:
-                out += ("}" * (indent_level - indents))
-                indent_level = indents
-                out += line
-                if line.strip()[-1] != ":":
-                    if line.strip()[-1] == ";":
-                        pass
-                    else:
-                        out += ";"
-            else:
-                out += line
-                if line.strip()[-1] != ":":
-                    if line.strip()[-1] == ";":
-                        pass
-                    else:
-                        out += ";"
-        else:
-            pass
-        out += "\n"
-    out += ("}" * (indent_level) + "\n")
-
-    return out
-
-def find_functions(raw):
-    out = re.findall(r"def ([a-zA-Z_0-9\.\[\]\:\*]+) (\w+)",raw)
-    return [i[1] for i in out if i[0] != 'cast' and i[1] != 'operator']
-
-def find_objects(raw):
-    return re.findall(r"object (\w+)",raw)
-
-def find_structs(raw):
-    return re.findall(r"struct (\w+)",raw)
-
-def find_namespaces(raw):
-    return re.findall(r"module (\w+)",raw)
-
-def get_lexer(filename = "tokens.txt", object_names = [], struct_names = [], function_names = [], namespaces = []):
+    Returns
+    -------
+    str
+        The input Moth code with all comments removed.
+    """
     
+    # Find all multiline comments (i.e., text enclosed between '###' and '###') in the code
+    multiline_comments = re.findall(r"\#\#\#.*?\#\#\#", code_str, re.DOTALL)
+    
+    # Replace each multiline comment in the code with newline characters, preserving line count
+    for comment in multiline_comments:
+        code_str = code_str.replace(comment, "\n" * comment.count("\n"))
+    
+    # Find all single-line comments (i.e., text following '#') in the code
+    single_line_comments = re.findall(r"\#.*", code_str)
+    
+    # Remove each single-line comment from the code
+    for comment in single_line_comments:
+        code_str = code_str.replace(comment, "")
+    
+    return code_str
+
+def convert_to_c_style(code_str):
+    """
+    Converts Python-style code (indentation-based) to C-style code (curly braces-based).
+
+    Parameters
+    ----------
+    code_str : str
+        A string representing the Python code to be converted.
+
+    Returns
+    -------
+    str
+        The input Python code converted into C-style code.
+    """
+
+    lines = code_str.splitlines()
+    indent_level = 0
+    output_code = ""
+
+    for line in lines:
+        # Skip empty lines
+        if len(line.strip()) == 0:
+            continue
+
+        indents = line.count("    ")  # Count the number of indentations (4 spaces)
+
+        if indents > indent_level:
+            # If current indent level is higher, add opening braces accordingly
+            output_code += "{" * (indents - indent_level)
+            indent_level = indents
+
+        elif indents < indent_level:
+            # If current indent level is lower, add closing braces accordingly
+            output_code += "}" * (indent_level - indents)
+            indent_level = indents
+
+        output_code += line
+
+        # Add semicolon at the end of the line if it's not a block opener (ends with :) 
+        # and doesn't already end with a semicolon
+        if not line.strip().endswith(":") and not line.strip().endswith(";"):
+            output_code += ";"
+
+        output_code += "\n"
+
+    # Add closing braces for any remaining open blocks
+    output_code += "}" * indent_level + "\n"
+
+    return output_code
+
+def find_functions(code_str):
+    """
+    Extracts the names of functions from a given Moth code string. 
+    It specifically ignores functions named 'operator' with with type
+    'cast'.
+
+    Parameters
+    ----------
+    code_str : str
+        A string representing the Python code from which to extract function names.
+
+    Returns
+    -------
+    list
+        A list of function names extracted from the input Python code.
+    """
+
+    # Regular expression pattern to match Moth function definitions.
+    # This pattern matches 'def' keyword, followed by type and then name 
+    function_matches = re.findall(r"def ([a-zA-Z_0-9\.\[\]\:\*]+) (\w+)", code_str)
+
+    # Return all function names except ones with type 'cast' and name 'operator'
+    function_names = [match[1] for match in function_matches if match[0] != 'cast' and match[1] != 'operator']
+
+    return function_names
+
+def find_objects(moth_code):
+    """
+    Extracts the names of objects from a given Moth code string.
+
+    Parameters
+    ----------
+    moth_code : str
+        A string representing the Moth code from which to extract object names.
+
+    Returns
+    -------
+    list
+        A list of object names extracted from the input Moth code.
+    """
+    return re.findall(r"object (\w+)", moth_code)
+
+def find_structs(moth_code):
+    """
+    Extracts the names of structs from a given Moth code string.
+
+    Parameters
+    ----------
+    moth_code : str
+        A string representing the Moth code from which to extract struct names.
+
+    Returns
+    -------
+    list
+        A list of struct names extracted from the input Moth code.
+    """
+    return re.findall(r"struct (\w+)", moth_code)
+
+def find_namespaces(moth_code):
+    """
+    Extracts the names of namespaces (modules) from a given Moth code string.
+
+    Parameters
+    ----------
+    moth_code : str
+        A string representing the Moth code from which to extract namespace names.
+
+    Returns
+    -------
+    list
+        A list of namespace names extracted from the input Moth code.
+    """
+    return re.findall(r"module (\w+)", moth_code)
+
+def build_moth_lexer(token_file="tokens.txt", object_names=[], struct_names=[], function_names=[], namespaces=[]):
+    """
+    Builds and returns a lexer for the Moth programming language.
+
+    Parameters
+    ----------
+    token_file : str, optional
+        The filename of the text file containing token definitions. Each line should contain a token type
+        and a regex pattern, separated by a space. Lines starting with '//' are ignored.
+        Default is "tokens.txt".
+
+    object_names : list of str, optional
+        A list of object names to be added to the lexer as OBJECT_NAME tokens.
+
+    struct_names : list of str, optional
+        A list of struct names to be added to the lexer as STRUCT_NAME tokens.
+
+    function_names : list of str, optional
+        A list of function names to be added to the lexer as FUNCTION_NAME tokens.
+
+    namespaces : list of str, optional
+        A list of namespace names to be added to the lexer as NAMESPACE tokens.
+
+    Returns
+    -------
+    LexerGenerator
+        The built lexer for the Moth programming language.
+    """
     lexer = LexerGenerator()
-    with open(filename,"r") as f:
-        raw = [i.split() for i in f.read().splitlines() if (not i.startswith("//")) and (not len(i) == 0)]
 
-    for i in object_names:
-        lexer.add("OBJECT_NAME",r"\b"+i+r"\b")
+    # Read the token definitions from the file
+    with open(token_file, "r") as f:
+        token_definitions = [line.split() for line in f.read().splitlines() 
+                             if not line.startswith("//") and len(line) != 0]
 
-    for i in struct_names:
-        lexer.add("STRUCT_NAME",r"\b"+i+r"\b")
+    # Add object name tokens to the lexer
+    for name in object_names:
+        lexer.add("OBJECT_NAME", r"\b" + re.escape(name) + r"\b")
 
-    for i in namespaces:
-        lexer.add("NAMESPACE",r"\b"+i+r"\b")
+    # Add struct name tokens to the lexer
+    for name in struct_names:
+        lexer.add("STRUCT_NAME", r"\b" + re.escape(name) + r"\b")
 
-    for i in function_names:
-        lexer.add("FUNCTION_NAME",r"\b"+i+r"\b")
+    # Add namespace tokens to the lexer
+    for name in namespaces:
+        lexer.add("NAMESPACE", r"\b" + re.escape(name) + r"\b")
 
-    for i in raw:
-        if i[0] == "IGNORE":
-            lexer.ignore(i[1])
+    # Add function name tokens to the lexer
+    for name in function_names:
+        lexer.add("FUNCTION_NAME", r"\b" + re.escape(name) + r"\b")
+
+    # Add or ignore other tokens as specified in the token file
+    for token_type, pattern in token_definitions:
+        if token_type == "IGNORE":
+            lexer.ignore(pattern)
         else:
-            lexer.add(i[0],i[1])
+            lexer.add(token_type, pattern)
 
     return lexer.build()
 
-def preprocess(fname : str, path, function_names = [],object_names = [],struct_names = [],namespaces = [],aliases = {}):
-    with open(fname,"r") as f:
-        raw = f.read()
-    raw = remove_comments(raw)
-    raw = convert(raw)
-    includes = re.findall(r"include\b.*\;",raw)
-    for i in includes:
-        raw = raw.replace(i,"")
-    includes = [i.split("include ")[1][:-1] for i in includes]
-    tokens = []
+def preprocess_file(filename, path, function_names=[], object_names=[], struct_names=[], namespaces=[], aliases={}):
+    """
+    Preprocesses a file of Moth code by removing comments, converting syntax, handling includes and aliases,
+    identifying functions, objects, structs, and namespaces, and tokenizing the code.
+
+    Parameters
+    ----------
+    filename : str
+        The path of the Moth code file to preprocess.
+
+    path : str
+        The path where the token file is located.
+
+    function_names : list of str, optional
+        A list of known function names.
+
+    object_names : list of str, optional
+        A list of known object names.
+
+    struct_names : list of str, optional
+        A list of known struct names.
+
+    namespaces : list of str, optional
+        A list of known namespace names.
+
+    aliases : dict, optional
+        A dictionary of known aliases, mapping alias names to their replacements.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the list of tokens generated from the preprocessed code, 
+        and the updated lists of function names, object names, struct names, namespace names,
+        and the aliases dictionary.
+    """
+    with open(filename,"r") as f:
+        code = f.read()
+
+    # Remove comments and convert to C-style syntax
+    code = remove_comments(code)
+    code = convert_to_c_style(code)
+
+    # Handle included files
+    includes = re.findall(r"include\b.*\;", code)
+    for include in includes:
+        code = code.replace(include, "")
+    include_files = [include.split("include ")[1][:-1] for include in includes]
+
     stdlib_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','..','stdlib'))
-    for i in includes:
-        name = i
-        if (i.startswith("<") and i.endswith(">")):
-            name = stdlib_path + "/" + i[1:-1]
-        this_tokens,this_function_names,this_object_names,this_struct_names,this_namespaces,this_aliases = preprocess(name,path)
-        aliases.update(this_aliases)
-        tokens += this_tokens
-        function_names += this_function_names
-        object_names += this_object_names
-        struct_names += this_struct_names
-        namespaces += this_namespaces
-    tmp_aliases = re.findall(r"alias\b.*\;",raw)
-    for i in tmp_aliases:
-        raw = raw.replace(i,"")
-    for i in tmp_aliases:
-        label = i.strip().split(" ")[1]
-        value = " ".join(i.strip().split(" ")[2:])[:-1]
+    tokens = []
+    for include_file in include_files:
+        if (include_file.startswith("<") and include_file.endswith(">")):
+            include_file = stdlib_path + "/" + include_file[1:-1]
+
+        processed = preprocess_file(include_file, path)
+        out_tokens, func_names, obj_names, struct_names, namespace_names, new_aliases = processed
+
+        tokens += out_tokens
+
+        aliases.update(new_aliases)
+        function_names += func_names
+        object_names += obj_names
+        struct_names += struct_names
+        namespaces += namespace_names
+
+    # Handle aliases
+    found_aliases = re.findall(r"alias\b.*\;", code)
+    for alias in found_aliases:
+        code = code.replace(alias, "")
+    for alias in found_aliases:
+        label, *value = alias.strip().split(" ")
+        value = " ".join(value)[:-1]
         aliases[label] = value
-    for label in aliases.keys():
-        val = aliases[label]
-        raw = re.sub(r"\b" + label.strip() + r"\b",val,raw)
+    for label, value in aliases.items():
+        code = re.sub(r"\b" + re.escape(label.strip()) + r"\b", value, code)
 
-    function_names += find_functions(raw)
-    object_names += find_objects(raw)
-    struct_names += find_structs(raw)
-    namespaces += find_namespaces(raw)
+    # Identify functions, objects, structs, and namespaces
+    function_names += find_functions(code)
+    object_names += find_objects(code)
+    struct_names += find_structs(code)
+    namespaces += find_namespaces(code)
 
-    lexer = get_lexer(filename = path, function_names = function_names, object_names = object_names, struct_names = struct_names, namespaces = namespaces)
+    # Tokenize the processed code
+    lexer = build_moth_lexer(token_file=path, function_names=function_names, object_names=object_names, struct_names=struct_names, namespaces=namespaces)
+    new_tokens = [token for token in lexer.lex(code)]
+    for token in new_tokens:
+        token.fileoforigin = filename
 
-    new_tokens = [i for i in lexer.lex(raw)]
-    for i in new_tokens:
-        i.fileoforigin = fname
-    tokens += new_tokens
-    return tokens,function_names,object_names,struct_names,namespaces,aliases
+    return tokens + new_tokens, function_names, object_names, struct_names, namespaces, aliases
 
-def lex(fname : str,path : str):
-    tokens,_,_,_,_,_ = preprocess(fname,path)
+def lex_file(filename, path):
+    """
+    Tokenizes a file of Moth code.
+
+    This function is designed to be called from the command-line interface.
+
+    Parameters
+    ----------
+    filename : str
+        The path of the Moth code file to tokenize.
+
+    path : str
+        The path where the token file is located.
+
+    Returns
+    -------
+    list
+        A list of tokens generated from the Moth code in the specified file.
+    """
+    tokens, *_ = preprocess_file(filename, path)
     return tokens
