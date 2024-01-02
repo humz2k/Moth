ce4e98c166e24e5f0125d7d53b24af8bd88889e9
Humza Qureshi <humzaqureshis@gmail.com>
2024-01-02 13:50:07 -0600

    asd

--- src/driver.c
+++ src/driver.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+int yyparse(void);
+int yylex(void);
+extern FILE* yyin;
+
+int main(int argc, char** argv){
+
+    if (argc == 1)return -1;
+
+    FILE* fp = fopen(argv[1], "r");
+    yyin = fp;
+    yyparse();
+    fclose(fp);
+
     return 0;
 }
